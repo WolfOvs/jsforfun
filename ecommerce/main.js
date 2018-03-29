c23a96fdd1194ceccadf697800cce0ec598fbc52
Chiozzi, Davide <davide.chiozzi@accenture.com>
2018-03-29 12:18:35 +0200

    code improve

--- ecommerce/main.js
+++ ecommerce/main.js
@@ -1,18 +1,42 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log( "ready!!" );
 });
 
 
 module = function(){
     var init = function(){
-
+        getProducts();
     };
+    var getProducts = function(){
+        const endpoint = 'http://localhost:3000/products';
+        let productsArray = [];
+        fetch(endpoint)
+        .then(blob => blob.json())
+        .then(data => {
+            console.log('data', data);
+            productsArray.push(data.data);
+        });
+
+        const productList = document.querySelector('.productList-container');
+
+        const html = productsArray.map(place => {
+            return `
+              <li>
+                <span>${place.name}, ${place.description}</span>
+                <span>${place.price}</span>
+              </li>
+            `;
+          }).join('');
+          productList.innerHTML = html;
+        console.log('products', productsArray, productList, html);
+    }
+
     var show = function(){
         console.log('show');
     };
     var hide = function(){
         console.log('hide');
     };
-    return{init:init, show:show, hide:hide}
+    return{init:init, show:show, hide:hide, getProducts:getProducts}
 }();
 module.init();
