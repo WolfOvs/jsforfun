c2e2663ddeaf275f2e2e11e6bf014a1c344b182e
agoman <agostino.mangiapia@accenture.com>
2018-03-29 11:12:43 +0200

    init function

--- ecommerce/main.js
+++ ecommerce/main.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+
+module = function(){
+    var init = function(){
+
+    };
+    var show = function(){
+        console.log('show');
+    };
+    var hide = function(){
+        console.log('hide');
+    };
+    return{init:init, show:show, hide:hide}
+}();
+module.init();
